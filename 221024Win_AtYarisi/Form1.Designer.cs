eb32dc44dbe24d96d4bc42a4cf6c9f49ffb31d90
Mustafa Ünal <mustafaunal074@gmail.com>
2022-10-24 16:44:01 +0300

    Add project files.

---
+++ 221024Win_AtYarisi/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace _221024Win_AtYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kellikinaye = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.delibey = new System.Windows.Forms.PictureBox();
+            this.akkacan = new System.Windows.Forms.PictureBox();
+            this.midas = new System.Windows.Forms.PictureBox();
+            this.karasimsek = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.yarışıBaşlatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkımızdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.kellikinaye)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.delibey)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.akkacan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.midas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.karasimsek)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kellikinaye
+            // 
+            this.kellikinaye.Enabled = false;
+            this.kellikinaye.Image = ((System.Drawing.Image)(resources.GetObject("kellikinaye.Image")));
+            this.kellikinaye.Location = new System.Drawing.Point(83, 42);
+            this.kellikinaye.Name = "kellikinaye";
+            this.kellikinaye.Size = new System.Drawing.Size(147, 106);
+            this.kellikinaye.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.kellikinaye.TabIndex = 0;
+            this.kellikinaye.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoEllipsis = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label1.Location = new System.Drawing.Point(702, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FİNİSH";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoEllipsis = true;
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(503, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 604);
+            this.label2.TabIndex = 2;
+            // 
+            // delibey
+            // 
+            this.delibey.Enabled = false;
+            this.delibey.Image = ((System.Drawing.Image)(resources.GetObject("delibey.Image")));
+            this.delibey.Location = new System.Drawing.Point(83, 141);
+            this.delibey.Name = "delibey";
+            this.delibey.Size = new System.Drawing.Size(147, 106);
+            this.delibey.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.delibey.TabIndex = 0;
+            this.delibey.TabStop = false;
+            // 
+            // akkacan
+            // 
+            this.akkacan.Enabled = false;
+            this.akkacan.Image = ((System.Drawing.Image)(resources.GetObject("akkacan.Image")));
+            this.akkacan.Location = new System.Drawing.Point(83, 253);
+            this.akkacan.Name = "akkacan";
+            this.akkacan.Size = new System.Drawing.Size(147, 106);
+            this.akkacan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.akkacan.TabIndex = 0;
+            this.akkacan.TabStop = false;
+            // 
+            // midas
+            // 
+            this.midas.Enabled = false;
+            this.midas.Image = ((System.Drawing.Image)(resources.GetObject("midas.Image")));
+            this.midas.Location = new System.Drawing.Point(83, 365);
+            this.midas.Name = "midas";
+            this.midas.Size = new System.Drawing.Size(147, 106);
+            this.midas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.midas.TabIndex = 0;
+            this.midas.TabStop = false;
+            // 
+            // karasimsek
+            // 
+            this.karasimsek.Enabled = false;
+            this.karasimsek.Image = ((System.Drawing.Image)(resources.GetObject("karasimsek.Image")));
+            this.karasimsek.Location = new System.Drawing.Point(83, 477);
+            this.karasimsek.Name = "karasimsek";
+            this.karasimsek.Size = new System.Drawing.Size(147, 106);
+            this.karasimsek.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.karasimsek.TabIndex = 0;
+            this.karasimsek.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 144);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(131, 28);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(130, 24);
+            this.toolStripMenuItem1.Text = "Yeniden";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yarışıBaşlatToolStripMenuItem,
+            this.hakkımızdaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(630, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // yarışıBaşlatToolStripMenuItem
+            // 
+            this.yarışıBaşlatToolStripMenuItem.Name = "yarışıBaşlatToolStripMenuItem";
+            this.yarışıBaşlatToolStripMenuItem.Size = new System.Drawing.Size(101, 24);
+            this.yarışıBaşlatToolStripMenuItem.Text = "Yarışı Başlat";
+            this.yarışıBaşlatToolStripMenuItem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hakkımızdaToolStripMenuItem
+            // 
+            this.hakkımızdaToolStripMenuItem.Name = "hakkımızdaToolStripMenuItem";
+            this.hakkımızdaToolStripMenuItem.Size = new System.Drawing.Size(101, 24);
+            this.hakkımızdaToolStripMenuItem.Text = "Hakkımızda";
+            this.hakkımızdaToolStripMenuItem.Click += new System.EventHandler(this.hakkımızdaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(630, 684);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.karasimsek);
+            this.Controls.Add(this.midas);
+            this.Controls.Add(this.akkacan);
+            this.Controls.Add(this.delibey);
+            this.Controls.Add(this.kellikinaye);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.kellikinaye)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.delibey)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.akkacan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.midas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.karasimsek)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox kellikinaye;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox delibey;
+        private System.Windows.Forms.PictureBox akkacan;
+        private System.Windows.Forms.PictureBox midas;
+        private System.Windows.Forms.PictureBox karasimsek;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem yarışıBaşlatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkımızdaToolStripMenuItem;
+    }
+}
+
---
+++ 221024Win_AtYarisi/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace _221024Win_AtYarisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            kellikinaye.Left += rnd.Next(1, 5);
+            delibey.Left += rnd.Next(1, 5);
+            akkacan.Left += rnd.Next(1, 5);
+            midas.Left += rnd.Next(1, 5);
+            karasimsek.Left += rnd.Next(1, 5);
+
+            AtKontrol(kellikinaye);
+            AtKontrol(delibey);
+            AtKontrol(akkacan);
+            AtKontrol(midas);
+            AtKontrol(karasimsek);
+        }
+        void AtKontrol(PictureBox at)
+        {
+            if (at.Right > label2.Left) // atlardan birinin kazandığını tespit edildiği yer
+            {
+                timer1.Stop();
+                AtDurumDegistir(false, at);
+                //sesci.Stop();
+                MessageBox.Show(at.Name + " Kazandı");
+            }
+        }
+        void AtDurumDegistir(bool durum, PictureBox kazanan)
+        {
+            kellikinaye.Enabled = delibey.Enabled = akkacan.Enabled = midas.Enabled = karasimsek.Enabled = durum;
+            kazanan.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            SoundPlayer sesci = new SoundPlayer();
+            sesci.SoundLocation = "ses.wav";
+            sesci.Play();
+            AtDurumDegistir(true, kellikinaye);
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //kellikinaye.Left = delibey.Left = akkacan.Left = midas.Left = karasimsek.Left = 83;
+            Application.Restart();
+        }
+
+        private void hakkımızdaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hakkimizda hayirlisi = new Hakkimizda();
+            hayirlisi.Show();
+        }
+    }
+}
